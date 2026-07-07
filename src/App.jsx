533b8e4a4dbc4957119095b117a194c52948bf10
Harisss132 <zaktwod123@gmail.com>
2026-07-07 12:02:25 +0700

    setup react vite + tailwindcss + shadcnUI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <Button> Test </Button>
+    </>
+  )
+}
+
+export default App
